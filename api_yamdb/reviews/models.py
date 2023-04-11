ec4edaf804d9145486b686e724245ff037679d58
Victor Tsyganov <tsyganov1989@gmail.com>
2023-04-11 13:48:19 +0300

    Add titles, categories and genres to models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,106 @@
 from django.db import models
 
-# Create your models here.
+from .validators import validate_year
+
+
+class Category(models.Model):
+    """ Модель описывающая категории """
+    name = models.CharField(
+        verbose_name='Название категории',
+        max_length=256
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор категории',
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(models.Model):
+    """ Модель описывающая жанры """
+    name = models.CharField(
+        verbose_name='Название жанра',
+        max_length=256
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор жанра',
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Title(models.Model):
+    """ Модель описывающая произведения """
+    name = models.CharField(
+        verbose_name='Название произведения',
+        max_length=256
+    )
+    year = models.PositiveIntegerField(
+        verbose_name='Год выхода',
+        validators=[validate_year]
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        null=True,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр',
+        through='GenreTitle'
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        null=True
+    )
+    rating = models.IntegerField(
+        verbose_name='Рейтинг',
+        null=True,
+        default=None
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+
+class GenreTitle(models.Model):
+    """ Модель описывающая множественные связи произведений и жанров """
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE)
+    genre = models.ForeignKey(
+        Genre,
+        verbose_name='Жанр',
+        on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}, жанр - {self.genre}'
+
+    class Meta:
+        verbose_name = 'Произведение и жанр'
+        verbose_name_plural = 'Произведения и жанры'
