270b3a85eb0d99b3b98cf746f207f49198b19f57
Victor Tsyganov <tsyganov1989@gmail.com>
2023-04-13 13:27:11 +0300

    Add Categories

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import CategoryViewSet
+
+app_name = 'api'
+
+router = routers.DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import filters
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from reviews.models import Category
+from .mixins import ListCreateDestroyViewSet
+from .serializers import CategorySerializer
+from .premissions import IsAdminOrReadOnly
+
+
+class CategoryViewSet(ListCreateDestroyViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+    pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
+    print('pagination_class', pagination_class)
