c57373fd3f1317bbdf5a17a69c382200750698fb
Victor Tsyganov <tsyganov1989@gmail.com>
2023-04-19 17:45:23 +0300

    Update requirements.txt

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -41,7 +41,7 @@ class GenreViewSet(CategoryViewSet):
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all().annotate(
         Avg('reviews__score')
-    ).order_by("name")
+    ).order_by('name')
     serializer_class = TitleSerializer
     permission_classes = (IsAdminOrReadOnly,)
     filter_backends = [DjangoFilterBackend]
